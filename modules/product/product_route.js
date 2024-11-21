cbdc444ce4750018cda22a72b429e4d578000448
pankajkoirala <pankaj.developer.77@gmail.com>
2024-11-21 17:22:15 +0545

    fix: issue

--- modules/product/product_route.js
+++ modules/product/product_route.js
@@ -3,10 +3,10 @@ import {
   getById,
   getAll,
 } from "./product_controller.js";
+
 const router = express.Router();
 
 router.get("/all", getAll);
 router.get("/:id", getById);
-
 
 export { router };
